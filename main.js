22398c42a0c45adcdcc577213d9a500c8a0577ee
crisisp <ex985179996@outlook.com>
2023-10-27 11:44:39 +0800

    Update main.js
    commit-test

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
+    
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
